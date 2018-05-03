bc4cb4248cbbb7b07630760b3937244cbef851d0
anabolic <itw9622@gmail.com>
2018-05-03 20:36:45 +0900

    2018.05.03 클라이언트 서버 안드로이드 연동

---
+++ ServerApp/app/src/main/java/com/example/student/serverapp/MainActivity2.java
@@ -0,0 +1,50 @@
+package com.example.student.serverapp;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView textView,textView2;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.textView);
+        textView2=findViewById(R.id.textView2);
+
+
+    }
+    public void click(View v){
+    String r=
+    }
+
+
+
+
+    //ServerSocket Start..................................
+
+
+    //ServerSocket End................................................
+
+
+}
